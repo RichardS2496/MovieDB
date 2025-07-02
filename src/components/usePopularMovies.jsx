9a09f07e793adba4f5542d2dd81ba5728f1bb21b
Richard Suarez <richardmsc24@gmail.com>
2025-07-02 11:28:37 +0200

    testing

--- src/components/usePopularMovies.jsx
+++ src/components/usePopularMovies.jsx
@@ -5,7 +5,7 @@ const fetcher = (url) =>
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization: `Bearer ${import.meta.env.VITE_BEARER_TOKEN}`,
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjNjZWY5ODk3NmIwNWE3NTNkYWRiNzY5OTk3YjZjMiIsIm5iZiI6MTcyNjk5MzU1NS44NDM0Miwic3ViIjoiNjVmOTY0NDkzM2EzNzYwMTg0MzZkZDA0Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.rDpcOn6p04i_F_M0F5l9S7tFwT840WiJVMGhkDJXxzU`,
     },
   }).then((res) => res.json());
 
